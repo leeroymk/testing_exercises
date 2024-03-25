27f75207e8ae310b58f6dc561403e2cf8c2253bd
leeroymk <demonic90@mail.ru>
2024-03-25 21:54:14 +0300

    refactor fourth test

--- tests/level_3/test_four_fraud.py
+++ tests/level_3/test_four_fraud.py
@@ -5,88 +5,92 @@ import pytest
 from functions.level_3.four_fraud import find_fraud_expenses
 
 
-def test__find_fraud_expenses__empty_history_return_empty_list():
+def test__find_fraud_expenses__no_fraud_empty_history():
     assert find_fraud_expenses([]) == []
 
 
 @pytest.mark.parametrize("amount", [3000, 5000])
-def test__find_fraud_expenses__over_3_payments_less_or_equal_5000_in_one_time_are_fraud(
+def test__find_fraud_expenses__fraud_found_over_3_payments_less_or_equal_5000_in_one_time(
     make_expense, amount
 ):
-    expense_1 = make_expense(
-        spent_in="chinar",
-        spent_at=datetime.datetime(2024, 3, 1),
-        amount=amount,
-    )
-    expense_2 = make_expense(
-        spent_in="chinar",
-        spent_at=datetime.datetime(2024, 3, 1),
-        amount=amount,
-    )
-    expense_3 = make_expense(
-        spent_in="chinar",
-        spent_at=datetime.datetime(2024, 3, 1),
-        amount=amount,
-    )
-
-    assert find_fraud_expenses([expense_1, expense_2, expense_3]) == [
-        expense_1,
-        expense_2,
-        expense_3,
+    expenses = [
+        make_expense(
+            spent_in="chinar",
+            spent_at=datetime.datetime(2024, 3, 1),
+            amount=amount,
+        ),
+        make_expense(
+            spent_in="chinar",
+            spent_at=datetime.datetime(2024, 3, 1),
+            amount=amount,
+        ),
+        make_expense(
+            spent_in="chinar",
+            spent_at=datetime.datetime(2024, 3, 1),
+            amount=amount,
+        ),
     ]
 
+    assert find_fraud_expenses(expenses) == expenses
 
-def test__find_fraud_expenses__over_3_payments_over_5000_in_one_time_are_not_fraud(
+
+def test__find_fraud_expenses__no_fraud_found_over_3_payments_over_5000_in_one_time(
     make_expense,
 ):
-    expense_1 = make_expense(
-        spent_in="chinar",
-        spent_at=datetime.datetime(2024, 3, 1),
-        amount=6000,
-    )
-    expense_2 = make_expense(
-        spent_in="chinar",
-        spent_at=datetime.datetime(2024, 3, 1),
-        amount=6000,
-    )
-    expense_3 = make_expense(
-        spent_in="chinar",
-        spent_at=datetime.datetime(2024, 3, 1),
-        amount=6000,
-    )
+    expenses = [
+        make_expense(
+            spent_in="chinar",
+            spent_at=datetime.datetime(2024, 3, 1),
+            amount=6000,
+        ),
+        make_expense(
+            spent_in="chinar",
+            spent_at=datetime.datetime(2024, 3, 1),
+            amount=6000,
+        ),
+        make_expense(
+            spent_in="chinar",
+            spent_at=datetime.datetime(2024, 3, 1),
+            amount=6000,
+        ),
+    ]
 
-    assert find_fraud_expenses([expense_1, expense_2, expense_3]) == []
+    assert find_fraud_expenses(expenses) == []
 
 
-def test__find_fraud_expenses__2_equal_purchases_below_max_amount_in_one_day_are_not_fraud(
+def test__find_fraud_expenses__no_fraud_found_2_equal_purchases_below_max_amount_in_one_day(
     make_expense,
 ):
-    expense_1 = make_expense(
-        amount=500,
-        spent_at=datetime.datetime(2024, 3, 1),
-    )
-    expense_2 = make_expense(
-        amount=500,
-        spent_at=datetime.datetime(2024, 3, 1),
-    )
+    expenses = [
+        make_expense(
+            amount=500,
+            spent_at=datetime.datetime(2024, 3, 1),
+        ),
+        make_expense(
+            amount=500,
+            spent_at=datetime.datetime(2024, 3, 1),
+        ),
+    ]
 
-    assert find_fraud_expenses([expense_1, expense_2]) == []
+    assert find_fraud_expenses(expenses) == []
 
 
-def test__find_fraud_expenses__3_purchases_below_max_amount_in_different_days_are_not_fraud(
+def test__find_fraud_expenses__no_fraud_found_3_purchases_below_max_amount_in_different_days(
     make_expense,
 ):
-    expense_1 = make_expense(
-        amount=500,
-        spent_at=datetime.datetime(2024, 3, 1),
-    )
-    expense_2 = make_expense(
-        amount=500,
-        spent_at=datetime.datetime(2024, 3, 2),
-    )
-    expense_3 = make_expense(
-        amount=500,
-        spent_at=datetime.datetime(2024, 3, 3),
-    )
+    expenses = [
+        make_expense(
+            amount=500,
+            spent_at=datetime.datetime(2024, 3, 1),
+        ),
+        make_expense(
+            amount=500,
+            spent_at=datetime.datetime(2024, 3, 2),
+        ),
+        make_expense(
+            amount=500,
+            spent_at=datetime.datetime(2024, 3, 3),
+        ),
+    ]
 
-    assert find_fraud_expenses([expense_1, expense_2, expense_3]) == []
+    assert find_fraud_expenses(expenses) == []
